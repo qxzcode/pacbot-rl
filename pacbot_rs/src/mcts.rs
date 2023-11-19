8f05ebe73ce6b25ab5e33afe7dea00551342f9ed
Quinn Tucker <qxzcraft@gmail.com>
2023-11-19 06:16:37 -0500

    Only store node, not edge, data

--- pacbot_rs/src/mcts.rs
+++ pacbot_rs/src/mcts.rs
@@ -1,3 +1,4 @@
+use itertools::izip;
 use ndarray::Array3;
 use num_enum::TryFromPrimitive;
 use numpy::{IntoPyArray, PyArray3};
@@ -12,64 +13,57 @@ type Return = f32;
 /// The factor used to discount future rewards each timestep.
 pub const DISCOUNT_FACTOR: f32 = 0.99;
 
-struct SearchTreeEdge {
-    policy_prior: f32,
-    visit_count: u32,
-    total_return: Return,
-    child: Option<Box<SearchTreeNode>>,
-}
-
-impl SearchTreeEdge {
-    /// Creates a new edge with the given policy prior probability.
-    #[must_use]
-    fn new(policy_prior: f32) -> Self {
-        Self { policy_prior, visit_count: 0, total_return: 0.0, child: None }
-    }
-
-    /// Returns the estimated expected return (cumulative reward) for this action.
-    #[must_use]
-    fn expected_return(&self) -> NotNan<f32> {
-        if self.visit_count == 0 {
-            NotNan::new(0.0).unwrap() // TODO: handle this case in a more principled way?
-        } else {
-            let expected_return = self.total_return / (self.visit_count as f32);
-            NotNan::new(expected_return).expect("expected return is NaN")
-        }
-    }
-
-    /// A variant of the PUCT score, similar to that used in AlphaZero.
-    #[must_use]
-    fn puct_score(&self, parent_visit_count: u32) -> NotNan<f32> {
-        let exploration_rate = 200.0; // TODO: make this a tunable parameter
-        let exploration_score = self.policy_prior
-            * ((parent_visit_count as f32).sqrt() / ((1 + self.visit_count) as f32));
-        self.expected_return() + exploration_rate * exploration_score
-    }
-}
-
+#[derive(Debug)]
 struct SearchTreeNode {
     visit_count: u32,
-    children: [SearchTreeEdge; 5],
+    total_return: Return,
+    policy_priors: [f32; 5],
+    children: [Option<Box<SearchTreeNode>>; 5],
 }
 
 impl SearchTreeNode {
-    /// Creates a new node with the given policy prior probabilities.
+    /// Creates a new node with the given policy prior probabilities and a visit_count of 1.
+    #[must_use]
+    fn new_visited(initial_evaluation: LeafEvaluation) -> Self {
+        Self {
+            visit_count: 1,
+            total_return: initial_evaluation.value,
+            policy_priors: initial_evaluation.policy,
+            children: Default::default(),
+        }
+    }
+
+    /// Creates a new unvisited node with the given policy prior probabilities.
     #[must_use]
     fn new(policy_priors: [f32; 5]) -> Self {
-        Self { visit_count: 0, children: policy_priors.map(SearchTreeEdge::new) }
+        Self { visit_count: 0, total_return: 0.0, policy_priors, children: Default::default() }
+    }
+
+    /// Creates a new, unvisited terminal node.
+    #[must_use]
+    fn new_terminal() -> Self {
+        Self::new([0.0; 5])
     }
 
     /// Returns the index of the next action to sample from the state, using the PUCT criterion.
     #[must_use]
     fn pick_action(&self, env: &PacmanGym) -> usize {
         // Choose a (valid) action based on the current stats.
-        let action_mask = env.action_mask();
-        let (action_index, _) = self
-            .children
-            .iter()
+        let (action_index, _) = izip!(env.action_mask(), &self.children, self.policy_priors)
             .enumerate()
-            .filter(|&(action_index, _)| action_mask[action_index])
-            .max_by_key(|(_, edge)| edge.puct_score(self.visit_count))
+            .filter(|&(_, (is_valid, _, _))| is_valid)
+            .max_by_key(|&(_, (_, child, prior))| {
+                // Compute the PUCT score for this child.
+                let exploration_rate = 200.0; // TODO: make this a tunable parameter
+
+                let child_visit_count = child.as_ref().map_or(0, |child| child.visit_count);
+                let child_value = self.child_value(child);
+
+                let exploration_score =
+                    prior * (self.visit_count as f32).sqrt() / ((1 + child_visit_count) as f32);
+
+                child_value + exploration_rate * exploration_score
+            })
             .unwrap();
         action_index
     }
@@ -77,27 +71,25 @@ impl SearchTreeNode {
     /// Returns the valid action with the highest expected return.
     #[must_use]
     fn best_action(&self, env: &PacmanGym) -> Action {
-        let action_mask = env.action_mask();
-        let (action_index, _) = self
-            .children
-            .iter()
+        let (action_index, _) = izip!(env.action_mask(), &self.children)
             .enumerate()
-            .filter(|&(action_index, _)| action_mask[action_index])
-            .max_by_key(|(_, edge)| edge.expected_return())
+            .filter(|&(_, (is_valid, _))| is_valid)
+            .max_by_key(|(_, (_, child))| self.child_value(child))
             .unwrap();
         Action::try_from_primitive(action_index.try_into().unwrap()).unwrap()
     }
 
     /// Returns the estimated expected return (cumulative reward) for this node/state.
     #[must_use]
-    fn expected_return(&self) -> Option<NotNan<f32>> {
-        if self.visit_count == 0 {
-            None
-        } else {
-            let total_return: Return = self.children.iter().map(|edge| edge.total_return).sum();
-            let expected_return = total_return / (self.visit_count as f32);
-            Some(NotNan::new(expected_return).expect("expected return is NaN"))
-        }
+    fn value(&self) -> NotNan<f32> {
+        let expected_return = self.total_return / self.visit_count as f32;
+        NotNan::new(expected_return).expect("expected return is NaN")
+    }
+
+    /// Returns the estimated expected return (cumulative reward) for a child of this node.
+    #[must_use]
+    fn child_value(&self, child: &Option<Box<SearchTreeNode>>) -> NotNan<f32> {
+        child.as_ref().map_or_else(|| self.value(), |child| child.value())
     }
 
     /// Returns the maximum node depth in this subtree.
@@ -105,8 +97,7 @@ impl SearchTreeNode {
     fn max_depth(&self) -> usize {
         self.children
             .iter()
-            .filter_map(|edge| edge.child.as_ref())
-            .map(|child| 1 + child.max_depth())
+            .filter_map(|child| child.as_ref().map(|child| 1 + child.max_depth()))
             .max()
             .unwrap_or(0)
     }
@@ -117,8 +108,7 @@ impl SearchTreeNode {
         1 + self
             .children
             .iter()
-            .filter_map(|edge| edge.child.as_ref())
-            .map(|child| child.node_count())
+            .filter_map(|child| child.as_ref().map(|child| child.node_count()))
             .sum::<usize>()
     }
 }
@@ -140,7 +130,7 @@ impl MCTSContext {
     #[new]
     pub fn new(env: PacmanGym, evaluator: PyObject) -> Self {
         let root_evaluation = eval_obs(env.obs(), &evaluator);
-        Self { env, root: SearchTreeNode::new(root_evaluation.policy), evaluator }
+        Self { env, root: SearchTreeNode::new_visited(root_evaluation), evaluator }
     }
 
     /// Resets the environment and search tree, starting a new episode.
@@ -150,7 +140,7 @@ impl MCTSContext {
 
         // Reset the search tree.
         let root_evaluation = eval_obs(self.env.obs(), &self.evaluator);
-        self.root = SearchTreeNode::new(root_evaluation.policy);
+        self.root = SearchTreeNode::new_visited(root_evaluation);
     }
 
     /// Takes the given action, stepping the environment and updating the search tree root to the
@@ -168,12 +158,11 @@ impl MCTSContext {
         if !done {
             // Update the root node.
             self.root = self.root.children[u8::from(action) as usize]
-                .child
                 .take()
                 .map(|child_box| *child_box)
                 .unwrap_or_else(|| {
                     let root_evaluation = eval_obs(self.env.obs(), &self.evaluator);
-                    SearchTreeNode::new(root_evaluation.policy)
+                    SearchTreeNode::new_visited(root_evaluation)
                 });
         }
 
@@ -187,23 +176,27 @@ impl MCTSContext {
     }
 
     /// Returns the action visit distribution at the root node.
+    ///
+    /// Panics if no children have been visited.
     #[must_use]
     pub fn action_distribution(&self) -> [f32; 5] {
-        array_init::map_array_init(&self.root.children, |edge| {
-            edge.visit_count as f32 / self.root.visit_count as f32
+        assert!(self.root.visit_count > 1);
+        let total_child_visits = (self.root.visit_count - 1) as f32;
+        array_init::map_array_init(&self.root.children, |child| {
+            child.as_ref().map_or(0.0, |child| child.visit_count as f32 / total_child_visits)
         })
     }
 
     /// Returns the estimated action (Q) values at the root node.
     #[must_use]
     pub fn action_values(&self) -> [f32; 5] {
-        array_init::map_array_init(&self.root.children, |edge| edge.expected_return().into())
+        array_init::map_array_init(&self.root.children, |child| self.root.child_value(child).into())
     }
 
     /// Returns the estimated value/return at the root node.
     #[must_use]
     pub fn value(&self) -> f32 {
-        self.root.expected_return().expect("no visits at root node").into()
+        self.root.value().into()
     }
 
     /// Performs MCTS iterations to grow the tree to (approximately) the given size,
@@ -240,11 +233,13 @@ impl MCTSContext {
     }
 }
 
+#[derive(Debug)]
 struct Transition {
     action_index: usize,
     reward: i32,
 }
 
+#[derive(Clone, Copy)]
 struct LeafEvaluation {
     value: Return,
     policy: [f32; 5],
@@ -277,8 +272,8 @@ impl MCTSContext {
             if done {
                 // We've reached a terminal state; stop.
                 return (trajectory, None);
-            } else if let Some(child_node) = &node.children[action_index].child {
-                // Update the current node and continue.
+            } else if let Some(child_node) = &node.children[action_index] {
+                // The child for the chosen action exists; update the current node and continue.
                 node = child_node;
             } else {
                 // We've reached an unexpanded node; stop and return the observation.
@@ -302,38 +297,38 @@ impl MCTSContext {
             // Get the next action edge.
             let (transition, rest) =
                 trajectory.split_first().expect("backprop called with empty trajectory");
-            let edge = &mut node.children[transition.action_index];
+            let child = &mut node.children[transition.action_index];
 
-            // (Recursively) get the subsequent return, then compute the return for this node.
-            let subsequent_return = {
-                if rest.is_empty() {
-                    // The trajectory ends here.
-                    if let Some(LeafEvaluation { value, policy }) = leaf_evaluation {
-                        // We have a leaf evaluation, so expand a new leaf node.
-                        edge.child = Some(Box::new(SearchTreeNode::new(policy)));
-                        value
-                    } else {
-                        // The trajectory reached a terminal node, so there is no child node and the
-                        // return from here is zero.
-                        0.0
-                    }
+            // Create/get the child node and the subsequent return.
+            let (child, subsequent_return) = if rest.is_empty() {
+                // The trajectory ends here.
+                if let Some(LeafEvaluation { value, policy }) = leaf_evaluation {
+                    // We have a leaf evaluation, so expand a new leaf node.
+                    assert!(child.is_none(), "tried to expand the same child twice");
+                    (child.insert(Box::new(SearchTreeNode::new(policy))), value)
                 } else {
-                    let child = edge.child.as_mut().expect("backprop hit an unexpanded child");
-                    backprop_helper(child, rest, leaf_evaluation)
+                    // The trajectory reached a terminal node, so the return from here is zero.
+                    (child.get_or_insert_with(|| Box::new(SearchTreeNode::new_terminal())), 0.0)
                 }
+            } else {
+                // Recursively get the subsequent return from the existing child.
+                let child = child.as_mut().expect("backprop hit an unexpanded child");
+                let subsequent_return = backprop_helper(child, rest, leaf_evaluation);
+                (child, subsequent_return)
             };
+
+            // Update the child node.
             let this_return = transition.reward as Return + DISCOUNT_FACTOR * subsequent_return;
+            child.visit_count += 1;
+            child.total_return += this_return;
 
-            // Update this node/edge.
-            node.visit_count += 1;
-            edge.visit_count += 1;
-            edge.total_return += this_return;
-
-            // Propagate the return up the trajectory.
+            // Propagate the return back up the trajectory.
             this_return
         }
 
-        backprop_helper(&mut self.root, trajectory, leaf_evaluation);
+        let subsequent_return = backprop_helper(&mut self.root, trajectory, leaf_evaluation);
+        self.root.visit_count += 1;
+        self.root.total_return += DISCOUNT_FACTOR * subsequent_return;
     }
 }
 
@@ -345,7 +340,7 @@ fn eval_obs(obs: Array3<f32>, evaluator: &PyObject) -> LeafEvaluation {
         let obs_numpy = obs.into_pyarray(py);
 
         // Run the evaluator.
-        let result = evaluator.call1(py, (obs_numpy,)).expect("PyTorch error");
+        let result = evaluator.call1(py, (obs_numpy,)).expect("error running evaluator");
 
         // Convert the result to Rust types.
         let (value, policy) = result.extract(py).expect("evaluator should return (value, policy)");
